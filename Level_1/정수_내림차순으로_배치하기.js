ffed1d965c52dccdae6cec5cff7fc7dd248ce6be
younjaewon <97930034+younjaewon@users.noreply.github.com>
2022-05-25 13:42:05 +0900

    프로그래머스 Level 1

---
+++ Level_1/정수_내림차순으로_배치하기.js
@@ -0,0 +1,27 @@
+function solution(n) {
+    var answer = 0;
+    var numArr = [];
+    
+    while(n>0) {
+        numArr.push(n%10);
+        n = parseInt(n/10, 10);
+    }
+    
+    numArr.sort((x,y) => y-x);
+    answer = Number(numArr.join(''));
+    
+    return answer;
+}
+
+
+/*
+문제 설명
+함수 solution은 정수 n을 매개변수로 입력받습니다. n의 각 자릿수를 큰것부터 작은 순으로 정렬한 새로운 정수를 리턴해주세요. 예를들어 n이 118372면 873211을 리턴하면 됩니다.
+
+제한 조건
+n은 1이상 8000000000 이하인 자연수입니다.
+
+입출력 예
+n	return
+118372	873211
+*/
